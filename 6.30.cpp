72e9e49d517a229eb0ee357c3a52011158479a35
刘佳祥 <3298115196@qq.com>
2021-09-17 15:12:58 +0800

    6-30

---
+++ 6.30.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int num = 0;
+	printf("请输入一个整数：");
+	scanf("%d", &num);
+	int i = i;
+	while (i <= num)
+	{
+		i++;
+		printf("%d\n", i);
+	}
+	return 0;
+}
